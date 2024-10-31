17e99705eed25d2efffdf570beecf6a99de4d34f
ChristopherMorriss <chrismorriss711@gmail.com>
2024-10-31 12:44:03 +0000

    Files have been setup but run/debug is not working

---
+++ car.cs
@@ -0,0 +1,22 @@
+class Car
+{
+    static void Main (string[] args)
+    {
+
+    }
+    public string carMake;
+    public string carModel;
+    public string carRegistration;
+    public string carYear;
+    public string carCurrentValue;
+
+    Car(string carMake,string carModel,string carRegistration, string carYear,string carCurrentValue)
+    {
+        CarMake = carMake;
+        CarModel = carModel;
+        CarRegistration = carRegistration;
+        CarYear = carYear;
+        CarCurrentValue = carCurrentValue;
+    }
+}
+console.WriteLine('Hello world!');
